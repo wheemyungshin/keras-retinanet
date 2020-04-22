9378de86470ea825bc2b065331916afa565c9e10
wheemyungshin <zerger26@gmail.com>
2020-04-22 07:28:36 +0000

    sgd

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -123,7 +123,7 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
             'regression'    : losses.smooth_l1(),
             'classification': losses.focal()
         },
-        optimizer=keras.optimizers.adam(lr=lr, clipnorm=0.001)
+        optimizer=keras.optimizers.SGD(lr=lr, decay=1e-4, momentum=0.9)
     )
 
     return model, training_model, prediction_model
@@ -431,6 +431,9 @@ def parse_args(args):
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file.')
     parser.add_argument('--weighted-average', help='Compute the mAP using the weighted average of precisions among classes.', action='store_true')
     parser.add_argument('--compute-val-loss', help='Compute validation loss during training', dest='compute_val_loss', action='store_true')
+    #parser.add_argument('--lr-decay',         help='Learning rate decay rate.', type=float, default=0)
+    #parser.add_argument('--lr-decay-step',    help='Learning rate decay step.', type=float, default=6)
+
 
     # Fit generator arguments
     parser.add_argument('--multiprocessing',  help='Use multiprocessing in fit_generator.', action='store_true')

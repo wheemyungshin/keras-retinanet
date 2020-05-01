a149e0377b1f3c79be2d25ec3510d398ab666381
wheemyungshin <zerger26@gmail.com>
2020-05-01 04:45:38 +0000

    no-resize for evaluation

--- keras_retinanet/bin/evaluate.py
+++ keras_retinanet/bin/evaluate.py
@@ -41,6 +41,7 @@ def create_generator(args, preprocess_image):
     """
     common_args = {
         'preprocess_image': preprocess_image,
+        'no_resize'        : args.no_resize
     }
 
     if args.dataset_type == 'coco':
@@ -111,6 +112,7 @@ def parse_args(args):
     parser.add_argument('--save-path',        help='Path for saving images with detections (doesn\'t work for COCO).')
     parser.add_argument('--image-min-side',   help='Rescale the image so the smallest side is min_side.', type=int, default=800)
     parser.add_argument('--image-max-side',   help='Rescale the image if the largest side is larger than max_side.', type=int, default=1333)
+    parser.add_argument('--no-resize',        help='Don''t rescale the image.', action='store_true')
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file (only used with --convert-model).')
 
     return parser.parse_args(args)
